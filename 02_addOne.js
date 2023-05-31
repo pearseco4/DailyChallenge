31d249f3b2fe8b387203afefa0e4bccacc9a8bb2
pearseodonohue <pearseodonohue@Pearses-MacBook-Air-2.local>
2023-05-31 12:09:32 -0400

    daily challenge 1 and 5

---
+++ 02_addOne.js
@@ -0,0 +1,16 @@
+/*-----------------------------------------------------------------
+Challenge: 01-addOne
+Difficulty: Basic
+Prompt:
+Write a function called addOne that takes a single number as an argument and returns that number plus 1.
+Examples:
+addOne(1) //=> 2
+addOne(-5) //=> -4
+-----------------------------------------------------------------*/
+// Your solution for 01-addOne here:
+
+function addOne(x) {
+    return x + 1;
+}
+
+console.log(addOne(5))
